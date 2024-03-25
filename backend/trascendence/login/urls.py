4a245df1a4732e46126047a701cf953aa9039b30
lorenzo <lorenzo@lorenzo.lorenzo.virtualbox.org>
2024-03-25 19:07:33 +0100

    dockerfile

--- backend/trascendence/login/urls.py
+++ backend/trascendence/login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("<str:extra_path>", views.index2)
 ]
--- backend/trascendence/login/views.py
+++ backend/trascendence/login/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.template.loader import render_to_string
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 
 def index(request):
-    response = render_to_string('login/login.html')
-    return HttpResponse(response)
+    try:
+        return render(request, 'login/login.html')
+    except:  
+        return render(request, 'login/prova.html')
+
+def index2(request, extra_path):
+    return render(request, 'login/prova.html')
+    
