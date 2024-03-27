2cf23d27c0cc9ab2e73ac22f246fa02aec886201
Itsimon98 <simo98ne@gmail.com>
2024-03-27 18:20:00 +0100

    NON FUNZIONA NULLA

--- backend/trascendence/login/views.py
+++ backend/trascendence/login/views.py
@@ -1,22 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login ,logout
 from django.template.loader import render_to_string
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
 
-def index(request):
-    if request.method == 'POST':
-        print(request.POST["username"])
-        return HttpResponseRedirect('/login/thank-you')
-    else: 
-        return render(request, "login/login.html")
+def thank_you(request):
+    return render(request,'login/thank-you.html')
+
 
 def index2(request, extra_path):
     return render(request, 'login/prova.html')
 
-def thank_you(request):
-    if request.method == 'POST':
-        print(request.POST["username"])
-        print(request.POST["password"]) 
-    return render(request, 'login/thank-you.html')
-    
+def index(request):
+
+    if request.method == "POST":
+        print("sono in index")
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print("sono qui")
+            return(redirect('thank-you'))
+        else:
+            print("ciao2")
+            return(redirect('login'))
+    else:
+        print("ciaooo")
+        return(render(request, 'login/login.html'))
+
+
