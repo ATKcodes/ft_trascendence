4954b3faaf34dcbe777ce4d2eeb0faf172776c04
lorenzo <lorenzo@lorenzo.lorenzo.virtualbox.org>
2024-03-28 16:29:52 +0100

    Authentication riuscita più o meno

--- backend/trascendence/login/views.py
+++ backend/trascendence/login/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.template.loader import render_to_string
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
 
 def index(request):
     if request.method == 'POST':
-        print(request.POST["username"])
-        return HttpResponseRedirect('/login/thank-you')
+        username = request.POST["username"]
+        password= request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/login/thank-you')
+        else:
+            return render(request, "login/login.html")
     else: 
         return render(request, "login/login.html")
 
@@ -15,8 +22,5 @@ def index2(request, extra_path):
     return render(request, 'login/prova.html')
 
 def thank_you(request):
-    if request.method == 'POST':
-        print(request.POST["username"])
-        print(request.POST["password"]) 
     return render(request, 'login/thank-you.html')
     
