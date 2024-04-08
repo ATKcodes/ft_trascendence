7eb3ff1f9041e34c0b2f4373ec36f3c34b107a43
lorenzo <lorenzo@lorenzo.lorenzo.virtualbox.org>
2024-04-08 17:40:03 +0200

    Aggiunto server a django

--- backend/trascendence/trascendence/settings.py
+++ backend/trascendence/trascendence/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p!cnv0z+m!cm5#h!#pwy4lz96rzi$80-gh0(7=-1gzt_7neh6h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'trascendence.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'template1',
+		'USER': 'postgres',
+		'PASSWORD': '1234',
+		'HOST' : 'localhost',
+		'PORT' : '5432',
     }
 }
 
