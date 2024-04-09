be7fe81b47e7dba60d2cab49e9d41faf23f3b631
Lorenzo Di mascia <ldi-masc@e4r9p2.42roma.it>
2024-04-09 18:57:48 +0200

    verificare database con django

--- backend/trascendence/trascendence/settings.py
+++ backend/trascendence/trascendence/settings.py
@@ -78,6 +78,12 @@ WSGI_APPLICATION = 'trascendence.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
