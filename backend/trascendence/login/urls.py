0675ca06f401f7cb45614a66835365ad48947346
lorenzo <lorenzo@lorenzo.lorenzo.virtualbox.org>
2024-04-10 19:31:15 +0200

    non funziona un cazz

--- backend/trascendence/login/urls.py
+++ backend/trascendence/login/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("users/", views.UserViewSet.as_view()),
     path("", views.index),
     path("thank-you", views.thank_you),
-    path("<str:extra_path>", views.index2)
 ]
--- backend/trascendence/login/views.py
+++ backend/trascendence/login/views.py
@@ -11,6 +11,41 @@ from . import serializers
 from django.shortcuts import get_object_or_404
 # Create your views here.
 
+class UserViewSet(APIView):
+    def get (self, request):
+        if id:
+            item = models.User.objects.get(id=id)
+            serializer = serializers.UserSerializer(item)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        
+        users = models.User.objects.all()
+        serializer = serializers.UserSerializer(users, many=True)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        item = models.User.objects.get(id=id)
+        serializer = serializers.UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, id=None):
+        item = models.User.objects.get(id=id)
+        serializer = serializers.UserSerializer(item, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete (self, request, id=None):
+        item = models.User.objects.filter(id=id)
+        print(item)
+        item.delete()
+        return Response({"status": "success", "data": "User deleted successfully"}, status=status.HTTP_200_OK)
+
+
+
 def index(request):
     if request.method == 'POST':
         username = request.POST["username"]
